814a5177498f46c44d09ba3cad541a676463fa2e
dpgian <dpgian@gmail.com>
2020-03-05 14:48:46 +0000

    Implemented add/delete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MaterialTable from 'material-table'
 
 function App() {
-  let data = [
+  let initialData = [
     {
       name: 'Almond',
       date: '12-20-20'
@@ -25,6 +25,8 @@ function App() {
     }
   ]
 
+  let [ data, setData ] = useState(initialData)
+
   // Enforces sort on date
   let sortDate = () => {
     let a = data.sort(function(a, b){
@@ -45,6 +47,39 @@ function App() {
           ]}
           data={data}
           title='My Table'
+          // actions={[
+          //   {
+          //     icon: 'add',
+          //     tooltip: 'Add Item',
+          //     isFreeAction: true,
+          //     onClick: (e) => console.log('Add')
+          //   },
+          //   {
+          //     icon: 'edit',
+          //     tooltip: 'Edit Item',
+          //     onClick: (e, row) => console.log(row)
+          //   }
+          // ]}
+          editable={{
+            onRowAdd: newItem => new Promise((res, rej) => {
+              setTimeout(() => {
+                setData([...data, newItem])
+                res()
+              }, 1000)
+            }),
+            onRowDelete: oldItem => new Promise((res, rej) => {
+              setTimeout(() => {
+                let i = data.indexOf(oldItem)
+                let temp = data
+                temp.splice(i, 1)
+                setData([...temp])
+                res()
+              }, 1000)
+            })
+          }}
+          options={{
+            actionsColumnIndex: -1
+          }}
         />
       </div>
     </>
